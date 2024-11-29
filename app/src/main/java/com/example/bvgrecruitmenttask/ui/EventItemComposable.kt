d64a92605085d2ca3b6de2ecbc7f468cc4caac9b
adamheinrich <adameksopot@gmail.com>
2024-11-29 09:27:17 +0100

    Improvements for Single Item

--- app/src/main/java/com/example/bvgrecruitmenttask/ui/EventItemComposable.kt
+++ app/src/main/java/com/example/bvgrecruitmenttask/ui/EventItemComposable.kt
@@ -1,21 +1,41 @@
 package com.example.bvgrecruitmenttask.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.bvgrecruitmenttask.domain.model.Event
 
 @Composable
 fun EventItem(event: Event) {
-    Column(modifier = Modifier.padding(8.dp)) {
-        event.id?.let { Text(text = "ID:${event.id}") }
-        event.createdAt?.let {
-            Text(text = "Created At: $it")
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = RoundedCornerShape(12.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            event.id?.let {
+                Text(text = "ID: ${event.id}")
+            }
+            event.createdAt?.let {
+                Text(
+                    text = "Created At: $it",
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
+                )
+            }
+            Text(text = "Type: ${event.eventType.name}")
+            Text(text = "Username: ${event.account?.username ?: "Unknown User"}")
         }
-        Text(text = "Type: ${event.eventType.name}")
-        Text(text = "Username: ${event.account?.username ?: "Unknown User"}")
     }
 }
